c05a2165c234f1168b4b4debbcf03ee4cb1a8952
seaky77 <seaky77@gmail.com>
2024-11-23 00:29:24 +0100

    v2.3.0

--- install.sh
+++ install.sh
@@ -1,22 +1,36 @@
 #!/bin/sh
 
+
+function stop_zgateway() {
+	pkill -9 -f "sh /vendor/bin/siliconlabs_host/guard_process.sh"
+    killall zgateway
+    killall mosquitto
+}
+
+function start_zgateway() {
+	sh /vendor/bin/siliconlabs_host/guard_process.sh &
+}
+
+
 DIR=$2
-echo "Firmware location is $DIR"
+echo "ZGateway location is $DIR"
 
 if [ ! -d "$DIR" ]; then
     echo "$DIR directory not found"
     exit 1
 fi
 
-if [ ! -f "$DIR/firmware_version" ]; then
+if [ ! -f "$DIR/zgateway_version" ]; then
 	if [ ! -d "${DIR}_original" ]; then
+		stop_zgateway
 		mv "$DIR" "${DIR}_original"
 		echo "Backup completed in the ${DIR}_original directory"
 	else
-		echo "Upps... firmware_version is missing but the backup already exists in the ${DIR}_original directory"
+		echo "Upps... zgateway_version is missing but the backup already exists in the ${DIR}_original directory"
 		exit 1
 	fi
 else 
+    stop_zgateway
 	echo "Purge $DIR"
     rm -f "$DIR/*"
 fi
@@ -27,3 +41,4 @@ cp -r "$1/." "$DIR"
 chmod -R 755 "$DIR"
 chown -R root:shell "$DIR"
 
+start_zgateway
