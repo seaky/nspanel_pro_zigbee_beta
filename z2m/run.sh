a5b2fe39a14bae948079edd4432b1a3a8dee7649
seaky77 <seaky77@gmail.com>
2024-11-27 18:56:12 +0100

    z2m

--- z2m/run.sh
+++ z2m/run.sh
@@ -4,12 +4,7 @@ echo "-------------Zigbee Host run-------------------------"
 LOCAL_TIME=$(date +"%Y-%m-%d %H:%M:%S")
 echo $LOCAL_TIME 'Zigbee Host, start' > /vendor/run.log 
 
-/vendor/bin/siliconlabs_host/:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=/data/local/tmp/nspanel_tools_pkg/nodejs/lib/:${LD_LIBRARY_PATH}
 echo ${LD_LIBRARY_PATH}
 
-killall mosquitto
-sleep 1
-
-
-/vendor/bin/siliconlabs_host/mosquitto -c /vendor/bin/siliconlabs_host/mosquitto.conf &
-
+/data/local/tmp/nspanel_tools_pkg/nodejs/bin/node /data/local/tmp/nspanel_tools_pkg/z2m/index.js
